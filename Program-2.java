b1a1356fbc65b6b527196ecf67224ea31601fe26
tabassum7463 <152901820+tabassum7463@users.noreply.github.com>
2025-07-20 23:02:34 +0530

    Create Program-2.java

---
+++ Program-2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class OddNumberSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of a: ");
+        int a = sc.nextInt();
+
+        for (int i = 0; i < a; i++) {
+            int oddNumber = 2 * i + 1;
+            System.out.print(oddNumber);
+            if (i < a - 1) {
+                System.out.print(", ");
+            }
+        }
+
+        sc.close();
+    }
+}
